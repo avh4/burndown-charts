dd5ba7b33c651f48c54248f25bb1b31db8bad726
Aaron VonderHaar <av@noredink.com>
2019-05-18 15:24:30 -0700

    Update example

--- example/Main.elm
+++ example/Main.elm
@@ -9,7 +9,6 @@ chartConfig : BurndownChart.Config
 chartConfig =
     { name = "MVP"
     , color = Just BurndownChart.blue
-    , pointsRemaining = [ 8, 8, 7, 7, 7, 24, 24, 24, 24, 24, 21, 21, 20, 22, 22, 14, 12, 11, 11, 11, 10, 8, 8, 8, 8, 8, 8, 7, 1 ]
     , startDate = ( 2019, Apr, 9 )
     , targetDate = ( 2019, May, 14 )
     , baseline = ( ( 2019, Apr, 17 ), 24 )
@@ -19,6 +18,7 @@ chartConfig =
         , ( "\u{1F57A}", 12, Just ( 2019, May, 1 ) )
         , ( "🏛", 0, Nothing )
         ]
+    , pointsRemaining = [ 8, 8, 7, 7, 7, 24, 24, 24, 24, 24, 21, 21, 20, 22, 22, 14, 12, 11, 11, 11, 10, 8, 8, 8, 8, 8, 8, 7, 1 ]
     }
 
 
